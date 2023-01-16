c4badf8e4de893dc904d26e0e0f9f7af5b7aa47c
Eduardo Kuhn <eduardo.kuhn@cwi.com.br>
2023-01-16 13:28:57 -0300

    Arrumando imports

--- app/src/main/java/br/com/eduardo/pocnavegacao/MainActivity.kt
+++ app/src/main/java/br/com/eduardo/pocnavegacao/MainActivity.kt
@@ -2,7 +2,6 @@ package br.com.eduardo.pocnavegacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 import br.com.eduardo.featurelogin.LoginRouter
 
 class MainActivity : AppCompatActivity() {
